639864f0bee496fc63df5a05d0912b26f18efb85
Ivar <vanobond1983@yandex.ru>
2018-11-22 15:24:51 +0400

    horizontal scroll

--- scrolledcanvas.py
+++ scrolledcanvas.py
@@ -8,13 +8,17 @@ class ScrolledCanvas(Frame):
         self.pack(expand=YES, fill=BOTH)
         canv = Canvas(self, bg=color, relief=SUNKEN)
         canv.config(width=300, height=200)
-        canv.config(scrollregion=(0, 0, 300, 10000))
+        canv.config(scrollregion=(0, 0, 1000, 10000))
         canv.config(highlightthickness=0)
         sbar = Scrollbar(self)
+        sbar_x = Scrollbar(self)
         sbar.config(command=canv.yview)
+        sbar_x.config(command=canv.xview, orient='horizontal')
         # связать sbar и canv
         canv.config(yscrollcommand=sbar.set) # сдвиг одного = сдвиг другого
         sbar.pack(side=RIGHT, fill=Y)
+        canv.config(xscrollcommand=sbar_x.set) # сдвиг одного = сдвиг другого
+        sbar_x.pack(side=BOTTOM, fill=X)
         # первым добавлен – посл. обрезан
         canv.pack(side=LEFT, expand=YES, fill=BOTH) # canv обрезается первым
         self.fillContent(canv)
